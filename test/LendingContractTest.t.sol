ee025266a9dce6562db3f97348dc38e1afc65174
David Zhang <125990317+Solidityarchitect@users.noreply.github.com>
2023-12-12 20:34:20 +0800

    Update LendingContractTest.t.sol

--- test/LendingContractTest.t.sol
+++ test/LendingContractTest.t.sol
@@ -8,10 +8,11 @@ import {HelperConfig} from "../script/HelperConfig.s.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract LendingContractTest is Test {
-    LendingContract public lendingContract;
+    LendingContract public lC;
     DeployLC public deployer;
     HelperConfig public helperConfig;
     address public usdc;
+    address public priceFeed;
 
     address public USER = makeAddr("user");
     address public LENDER = makeAddr("lender");
@@ -20,8 +21,8 @@ contract LendingContractTest is Test {
 
     function setUp() external {
         deployer = new DeployLC();
-        (lendingContract, helperConfig) = deployer.run();
-        (, usdc,) = helperConfig.activeNetworkConfig();
+        (lC, helperConfig) = deployer.run();
+        (usdc, priceFeed,) = helperConfig.activeNetworkConfig();
         ERC20Mock(usdc).mint(USER, 1000);
         ERC20Mock(usdc).mint(LENDER, 1000);
 
@@ -29,59 +30,116 @@ contract LendingContractTest is Test {
         console.log(ERC20Mock(usdc).balanceOf(LENDER));
     }
 
-    // function testRequestLoan() external {
-    //     vm.startPrank(USER);
-    //     orderId = 1;
-    //     uint256 amount = 500;
-    //     uint256 loanTerm = 30 days;
-    //     lendingContract.requestLoan(orderId, amount, loanTerm);
-    //     vm.stopPrank();
-    // }
+    // Main Function Test
+    function testRequestLoan() external {
+        vm.startPrank(USER);
+        orderId = 1;
+        uint256 amount = 500;
+        uint256 loanTerm = 6000;
+        lC.requestLoan(orderId, amount, loanTerm, LENDER);
 
-    // function testAddLender() external {
-    //     vm.startPrank(USER);
-    //     uint256 amount = 200;
-    //     lendingContract.addLender(orderId, LENDER, amount);
-    //     assertEq(lendingContract.nameToAmount(LENDER), amount, "The amount should match");
-    //     vm.stopPrank();
-    // }
+        // Assert
+        LendingContract.LoanRequest memory loanRequest = lC.getLoanRequests(USER);
+        assertEq(loanRequest.orderId, orderId, "Order ID mismatch");
+        assertEq(loanRequest.amount, amount, "Amount mismatch");
+        assertEq(loanRequest.loanTerm, loanTerm, "Loan term mismatch");
 
-    // modifier requestLoan() {
-    //     vm.startPrank(USER);
-    //     orderId = 1;
-    //     uint256 amount = 500;
-    //     uint256 loanTerm = 600;
-    //     lendingContract.requestLoan(orderId, amount, loanTerm);
-    //     vm.stopPrank();
-    //     _;
-    // }
+        vm.stopPrank();
+    }
 
-    // modifier addLender() {
-    //     vm.startPrank(USER);
-    //     uint256 amount = 200;
-    //     lendingContract.addLender(orderId, LENDER, amount);
-    //     assertEq(lendingContract.nameToAmount(LENDER), amount, "The amount should match");
-    //     vm.stopPrank();
-    //     _;
-    // }
+    function testLending() external {
+        vm.startPrank(USER);
+        orderId = 1;
+        uint256 amount = 500;
+        uint256 loanTerm = 6000;
+        lC.requestLoan(orderId, amount, loanTerm, LENDER);
+        vm.stopPrank();
 
-    // function testLending() external {
-    //     vm.startPrank(USER);
-    //     uint256 amount = 200;
-    //     ERC20Mock(usdc).approve(address(this), amount);
-    //     lendingContract.lending(orderId, amount, USER);
-    //     // ERC20Mock(usdc).transferFrom(lender, user, amount);
-    //     // assertEq(lendingContract.balances(msg.sender), amount, "The balance should match");
-    //     vm.stopPrank();
-    // }
+        vm.startPrank(LENDER);
+        ERC20Mock(usdc).approve(address(lC), amount);
+        lC.lending(orderId, amount, USER);
 
-    // function testRepay() external requestLoan addLender {
-    //     vm.startPrank(LENDER);
-    //     uint256 amount = 200;
-    //     orderId = 1;
-    //     ERC20Mock(usdc).approve(address(this), amount);
-    //     lendingContract.repay(orderId, amount, LENDER);
-    //     // assertEq(lendingContract.balances(lender), 0, "The balance should be zero after repayment");
-    //     vm.stopPrank();
-    // }
+        // Assert
+        uint256 lenderBalance = lC.getLenderBalance(LENDER);
+        assertEq(lenderBalance, 500, "Lender balance incorrect after lending");
+
+        vm.stopPrank();
+    }
+
+    modifier requestLoanAndLending() {
+        vm.startPrank(USER);
+        orderId = 1;
+        uint256 amount = 500;
+        uint256 loanTerm = 6000;
+        lC.requestLoan(orderId, amount, loanTerm, LENDER);
+        vm.stopPrank();
+
+        vm.startPrank(LENDER);
+        ERC20Mock(usdc).approve(address(lC), amount);
+        lC.lending(orderId, amount, USER);
+        vm.stopPrank();
+        _;
+    }
+
+    function testRepay() external requestLoanAndLending {
+        uint256 preRepayBalance = lC.getLenderBalance(LENDER);
+
+        vm.startPrank(USER);
+        uint256 amount = 500;
+        orderId = 1;
+        ERC20Mock(usdc).approve(address(lC), amount);
+        lC.repay(orderId, amount, LENDER);
+
+        // Assert
+        uint256 postRepayBalance = lC.getLenderBalance(LENDER);
+        assertEq(postRepayBalance, preRepayBalance - 500, "Lender balance incorrect after repayment");
+        vm.stopPrank();
+    }
+
+    function testRemainingLoanTerm() external requestLoanAndLending {
+        vm.startPrank(USER);
+        orderId = 1;
+        uint256 remainingTerm = lC.remainingLoanTerm(orderId);
+        vm.stopPrank();
+
+        // Assert
+        assertTrue(remainingTerm <= 6000, "Remaining loan term should be less or equal to the original term");
+    }
+
+    // Getter Function Test
+    function testGetLoanRequest() external requestLoanAndLending {
+        uint256 loanAmountUSD = lC.getLoanRequest(USER);
+        console.log("Loan amount in USD:", loanAmountUSD);
+
+        // Assert USDC/USD 1:1
+        assertTrue(loanAmountUSD >= 1, "Loan amount in USD should be greater than 0");
+    }
+
+    function testGetRemainingLoanTerms() external requestLoanAndLending {
+        LendingContract.RemainingLoanTime memory remainingLoanTerm = lC.getRemainingLoanTerms(orderId);
+        assertTrue(remainingLoanTerm.loanTerm <= 6000, "Loan term should be less or equal to initial term");
+        assertEq(remainingLoanTerm.borrower, USER, "Borrower address mismatch");
+    }
+
+    function testGetNameToAmount() external requestLoanAndLending {
+        uint256 amountUSD = lC.getNameToAmount(LENDER);
+        console.log("Name to amount in USD:", amountUSD);
+        assertTrue(amountUSD == 500, "Amount in USD should be greater than 0");
+    }
+
+    function testGetBorrowers() external requestLoanAndLending {
+        address borrowerAddress = lC.getBorrowers(0);
+        assertEq(borrowerAddress, USER, "Borrower address mismatch");
+    }
+
+    function testGetLenders() external requestLoanAndLending {
+        LendingContract.Lenders memory lender = lC.getLenders(0);
+        assertEq(lender.lender, LENDER, "Lender address mismatch");
+        assertEq(lender.amount, 500, "Lender amount mismatch");
+    }
+
+    function testGetPriceFeed() external {
+        address priceFeedAddress = address(lC.getPriceFeed());
+        assertEq(priceFeedAddress, priceFeed, "Price feed address mismatch");
+    }
 }
