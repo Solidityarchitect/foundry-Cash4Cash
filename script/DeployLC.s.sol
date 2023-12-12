b580c0e204b7e5881c55b7d40d5fbaaaa7e28721
David Zhang <125990317+Solidityarchitect@users.noreply.github.com>
2023-12-12 20:34:44 +0800

    Update DeployLC.s.sol

--- script/DeployLC.s.sol
+++ script/DeployLC.s.sol
@@ -8,10 +8,10 @@ import {LendingContract} from "../src/LendingContract.sol";
 contract DeployLC is Script {
     function run() external returns (LendingContract, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
-        (address priceFeed, address usdc, uint256 deployerKey) = helperConfig.activeNetworkConfig();
+        (address usdc, address priceFeed, uint256 deployerKey) = helperConfig.activeNetworkConfig();
 
         vm.startBroadcast(deployerKey);
-        LendingContract lendingContract = new LendingContract(priceFeed, usdc);
+        LendingContract lendingContract = new LendingContract(usdc,priceFeed);
         vm.stopBroadcast();
         return (lendingContract, helperConfig);
     }
